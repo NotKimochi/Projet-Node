b3a51e3ba48e4f77b43476c9be935d51ce326c55
NikosThoumyre <nikosthoumyre@gmail.com>
2024-12-19 19:22:44 +0100

    création entité produit

---
+++ server/models/produit.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require("sequelize");
+const connection = require("./db");
+
+class Produit extends Model {}
+
+Produit.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    prix: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: 0,
+      },
+    },
+    rayon: {
+      type: DataTypes.INTEGER,
+    },
+    quantité: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize: connection,
+    modelName: "Produit",
+  }
+);
+
+module.exports = Produit;
